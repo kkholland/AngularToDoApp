4446950c1a846c38b9de476022475232ba09eba9
kkholland <katherinebholland@gmail.com>
2021-03-31 18:30:39 -0400

    inital commit -html, styles, and ts for todo component

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { ToDo } from './todo';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+
+export class TodoComponent implements OnInit {
+  newTask: string = ''
+  filterTask: string = ''
+
+  tasks: ToDo[] = [
+    {task: 'fold clothes',
+    complete: false},
+    {task: 'put clothes in dresser',
+    complete: false},
+    {task: 'relax',
+    complete: false},
+    {task: 'try to pet cat',
+    complete: true},
+    {task: 'pet dog',
+    complete: false},
+    {task: 'be awesome',
+    complete: false}
+  ]
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addTask(){
+    this.tasks.push({
+      task: this.newTask,
+      complete: false,
+    });
+    this.newTask = '';
+  }
+
+  removeTask(i:number){
+    this.tasks.splice(i,1);
+  }
+
+}
